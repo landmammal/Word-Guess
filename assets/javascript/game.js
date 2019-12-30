cb682c67bea71cc98aa8d655995ebbad202266b4
Alex Chu <chu.alex9102@gmail.com>
2019-12-30 17:35:59 -0500

    created wins, losses, chosenWord variables, roll function to chose a word from wordBank

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,22 @@
 //theme is movies
 
-let wordBank = ["avatar", "titanic", "avengers", "star wars", "jurassicpark", "lionking", "blackpanther", "frozen", "minions", "joker"]
+let wordBank = ["avatar", "titanic", "avengers", "starwars", "jurassicpark", "lionking", "blackpanther", "frozen", "minions", "joker"]
+
+let wins = 0;
+let losses = 0; 
+let chosenWord;
+
+function roll () {
+    index = Math.floor(Math.random() * 10)
+    chosenWord = wordBank[index];
+
+    for (let i = 0; i <= wordBank.length; i++) {
+
+    }
+}
+
+roll();
+
+console.log(chosenWord);
+
 
