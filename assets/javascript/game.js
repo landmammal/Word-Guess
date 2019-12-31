67113ba9b30b7d67ac98599227d1a65fb9a3437b
Alex Chu <chu.alex9102@gmail.com>
2019-12-30 20:04:17 -0500

    added document innerhtmls for wins,loss, and guesses left to rerefresh when start button is clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,17 @@ let wordBank = ["avatar", "titanic", "avengers", "starwars", "jurassicpark", "li
 
 let wins = 0;
 let losses = 0; 
+let guessLeft = 0; 
 let chosenWord;
+let spacedWord = ""; 
 let hiddenWord = "";
-
-function roll () {
-    index = Math.floor(Math.random() * 10)
-    chosenWord = wordBank[index];
-
-    for (let i = 0; i <= chosenWord.length; i++) {
-        hiddenWord += "_ "
-        document.getElementById("currentWord").innerHTML = hiddenWord;
-    }
-    
-console.log(chosenWord);
-}
 
 function clear(){
     hiddenWord = "";
     document.getElementById("currentWord").innerText = hiddenWord;
+    document.getElementById("wins").innerText = "Wins: " + wins;
+    document.getElementById("losses").innerText = "Losses: " + losses;
+    document.getElementById("GuessesLeft").innerText = "Guesses Left: " + guessLeft;
 }; 
 
 document.getElementById("start").onclick = function() {
@@ -29,13 +22,22 @@ document.getElementById("start").onclick = function() {
     roll();
 };
 
+function roll () {
+    index = Math.floor(Math.random() * 10)
+    chosenWord = wordBank[index];
 
-
-
-document.addEventListener('keyup', check);
-
-function check(e) {
-  console.log(e.key);
+    for (let i = 0; i < chosenWord.length; i++) {
+        hiddenWord += "_ ";
+        document.getElementById("currentWord").innerHTML = hiddenWord;
+    }
+    for (let j = 0; j <= chosenWord.length-1; j++) {
+        spacedWord += chosenWord.charAt(j) + " ";
+    }
+    
+console.log(chosenWord);
+console.log(spacedWord);
 }
+
+
 
 
