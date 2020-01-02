4036a252d1200c7561c94e5c9e1a66cc2096f932
Alex Chu <chu.alex9102@gmail.com>
2020-01-02 14:33:46 -0500

    fixed error where last guess from previous game would be used as first guess of next game by taking out clear functions in win/loss conditions and using the play again function for new game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,20 +14,21 @@ function clear(){
     hiddenWord = "";
     lettersGuessed = [];
     unique = [];
-    let guessLeft = 10; 
+    guessLeft = 10; 
     document.getElementById("lettersGuessed").innerText = lettersGuessed;
     document.getElementById("wins").innerText = "Wins: " + wins;
     document.getElementById("losses").innerText = "Losses: " + losses;
-    document.getElementById("GuessesLeft").innerText = "Guesses Left: " + guessLeft;
+    document.getElementById("GuessesLeft").innerHTML = guessLeft;
 }; 
 
 document.getElementById("start").onclick = function() {
     clear();
-    roll();
     playAgain();
+    roll();
 };
 
 function roll () {
+    document.addEventListener('keyup', check);
     index = Math.floor(Math.random() * 10)
     chosenWord = wordBank[index];
     splitWordArr = [];
@@ -47,37 +48,40 @@ function roll () {
     document.getElementById("currentWord").innerHTML = hiddenWord;
 }
 
-document.addEventListener('keyup', check);
+// document.addEventListener('keyup', check);
 
 
 function check(e) {
     var x = event.keyCode;  
     var y = String.fromCharCode(x).toLowerCase();
 
+        //bug 1, need to come up with a solution to not include correct letters into the letters guess array
         for (let k = 0; k < splitWordArr.length; k++) {
             if(y === splitWordArr[k]) {
                 hiddenWordArr[k] = splitWordArr[k];
                 hiddenWord = hiddenWordArr.join(" ");
                 document.getElementById("currentWord").innerHTML = hiddenWord; 
                 lettersGuessed.push(y);
-            } else if (hiddenWordArr.indexOf("_") === -1) {
-                alert("You Won!")
-                wins += 1
-                document.getElementById("wins").innerHTML = "Wins: " + wins;
-                clear();
-                roll();
+            } 
+
+            //bug 2, game automatically uses the last guess from previous game as the first guess of the next game after a win. May have something to do with index -1 condition
+            if (hiddenWordArr.indexOf("_") === -1) {
+                if (confirm("You Won")) {           
+                    wins += 1
+                    document.getElementById("GuessesLeft").innerHTML = guessLeft;
+                    document.getElementById("wins").innerHTML = "Wins: " + wins;}
             }
         }
+
         if (lettersGuessed.indexOf(y) === -1){
             guessLeft -= 1;
             lettersGuessed.push(y);
-            document.getElementById("GuessesLeft").innerHTML = "Guesses Left: " + guessLeft; 
+            document.getElementById("GuessesLeft").innerHTML = guessLeft; 
                 if (guessLeft === 0) {
                     alert("You Lost!")
                     losses += 1
-                    document.getElementById("losses").innerHTML = "Losses: " + wins;
-                    clear();
-                    roll(); 
+                    document.getElementById("losses").innerHTML = "Losses: " + wins;   
+                    // clear();
                 } 
         }
     let unique = [...new Set(lettersGuessed)];
